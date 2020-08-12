159cd140cea7ab78f2451715d34e88a096a63924
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-08-12 14:25:27 +1000

    update APIUrl to point to my netlify identity server

--- src/App.vue
+++ src/App.vue
@@ -47,8 +47,8 @@ import netlifyIdentity from "netlify-identity-widget";
 import { mapGetters, mapActions } from "vuex";
 
 netlifyIdentity.init({
-  APIUrl: "https://netlify-identity-demo-vue.netlify.com/.netlify/identity",
-  logo: true // you can try false and see what happens
+  APIUrl: "https://zen-ardinghelli-8b83b2.netlify.app/.netlify/identity",
+  logo: true, // you can try false and see what happens
 });
 
 export default {
@@ -56,51 +56,51 @@ export default {
   components: {
     Home,
     Public,
-    Protected
+    Protected,
   },
   computed: {
     ...mapGetters("user", {
       isLoggedIn: "getUserStatus",
-      user: "getUser"
+      user: "getUser",
     }),
     username() {
       return this.user ? this.user.username : ", there!";
-    }
+    },
   },
   data: () => ({
-    currentUser: null
+    currentUser: null,
   }),
   methods: {
     ...mapActions("user", {
-      updateUser: "updateUser"
+      updateUser: "updateUser",
     }),
     triggerNetlifyIdentityAction(action) {
       if (action == "login" || action == "signup") {
         netlifyIdentity.open(action);
-        netlifyIdentity.on(action, user => {
+        netlifyIdentity.on(action, (user) => {
           this.currentUser = {
             username: user.user_metadata.full_name,
             email: user.email,
             access_token: user.token.access_token,
             expires_at: user.token.expires_at,
             refresh_token: user.token.refresh_token,
-            token_type: user.token.token_type
+            token_type: user.token.token_type,
           };
           this.updateUser({
-            currentUser: this.currentUser
+            currentUser: this.currentUser,
           });
           netlifyIdentity.close();
         });
       } else if (action == "logout") {
         this.currentUser = null;
         this.updateUser({
-          currentUser: this.currentUser
+          currentUser: this.currentUser,
         });
         netlifyIdentity.logout();
         this.$router.push({ name: "Home" });
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
